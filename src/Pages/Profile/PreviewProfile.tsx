55a81608aeed4df88aa4a41671e3d5bc425ab1bb
bril95 <bril.vadim@mail.ru>
2024-11-12 13:17:48 +0200

    fill PreviewProfile

--- src/Pages/Profile/PreviewProfile.tsx
+++ src/Pages/Profile/PreviewProfile.tsx
@@ -1,14 +1,28 @@
 import { Box, Button, Typography } from "@mui/material";
+import cardMedia from '../../assets/сardMedia.svg'
 
 const PreviewProfile = () => {
   return (
     <Box sx={{
-      width: '320px'
+      width: '320px',
+      p: '20px',
     }}>
-      <Box>Photo</Box>
-      <Typography>ФИО</Typography>
-      <Typography>Статус</Typography>
-      <Button>Выйти из аккаунта</Button>
+      <img src={cardMedia} alt='Картинка профиля' style={{ width: '320px', height: '240px' }} />
+      <Typography sx={{mt: '20px', mb: '10px'}}>Ангелина Фомина</Typography>
+    <Typography>
+      <Typography component='span' fontWeight='bold'>Статус:</Typography> Начинающий
+    </Typography>
+      <Button sx={{
+        borderColor: 'black',
+        borderWidth: '1px',
+        borderStyle: 'solid',
+        color: 'black',
+        width: '100%',
+        height: '42px',
+        mt: '50px',
+      }}>
+        Выйти из аккаунта
+      </Button>
     </Box>
   )
 }
