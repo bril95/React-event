d89e893f279aa61fc380cc6b52d0da00ecaf4b4f
bril95 <bril.vadim@mail.ru>
2024-12-03 17:20:31 +0200

    add text in ErrorPage

--- src/Pages/common/ErrorPage.tsx
+++ src/Pages/common/ErrorPage.tsx
@@ -1,10 +1,11 @@
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import errorInfo from "../../assets/errorInfo.svg";
 
 const ErrorPage = () => {
   return (
     <Box>
       <img src={errorInfo} alt="errorInfo" />
+      <Typography variant="h6">Ошибка! Не удалось загрузить запросы</Typography>
     </Box>
   )
 };
